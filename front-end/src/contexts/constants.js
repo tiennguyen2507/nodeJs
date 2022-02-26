8d8bcbc71e2fd030aefb3f821058788ab23664eb
tien nguyen <nguyenledinhtien2507@gmail.com>
2022-02-26 22:14:51 +0700

    fix mini

--- front-end/src/contexts/constants.js
+++ front-end/src/contexts/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'somedeploy'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://peaceful-basin-18321.herokuapp.com/api' : 'http://localhost:5000/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
